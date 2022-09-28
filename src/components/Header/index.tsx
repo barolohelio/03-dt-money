16a9267493d461d185bb33e28afe17fe20655caf
Helio Barolo <barolohelio@gmail.com>
2022-09-28 08:05:07 -0300

    Atualizando Logo, e aplicando useContextSelector

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import * as Dialog from '@radix-ui/react-dialog'
 
-import logImg from '../../assets/logo.svg'
+import logImg from '../../assets/logoPiggys.svg'
 import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,8 +1,10 @@
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionsContext'
 
 export function useSummary() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
   // reduce percorre um Array e vai reduzir a uma nova estrutura de dados
   // Neste caso vai ter [{income: 0, outcome:0 , total: 0}]
